9161b521b9f8c020cc0a01dc6636fce9bdbac356
qoalstn44 <qoalstn44@naver.com>
2022-12-26 19:24:17 +0900

    Update PostForm Update PostModule Add PostForm useStare Add onChangeTitle Add onChangeBody Add dispatch

--- src/Pages/PostForm.jsx
+++ src/Pages/PostForm.jsx
@@ -3,13 +3,18 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import { Editor as ToastEditor } from '@toast-ui/react-editor';
+import { useRef } from 'react';
 
-// 설명: toast-ui color picker 플러그인
-
+// 설명: useState
+import { useState } from 'react';
+// 설명: modules
+import { addPost, getPosts } from '../redux/modules/postModule';
 // 설명: StHeader, StItemSlider 스타일링
 import logo from '../img/logo.png';
 import React from 'react';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
 
 const onClickPostList = () => {
   window.location.href = '/postlist';
@@ -24,8 +29,31 @@ const onRemove = () => {
     alert('삭제가 취소되었습니다.');
   }
 };
-
+const onCompletion = () => {};
+// 설명: useState
 function PostForm() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const onChangeTitle = () => {
+    const data = editorRef.current?.getInstance().getMarkdown();
+    setTitle(data);
+  };
+  const onChangeBody = () => {
+    const data = editorRef.current?.getInstance().getMarkdown();
+    setBody(data);
+  };
+  const dispatch = useDispatch();
+  const onClick = () => {
+    if (window.confirm('정말 등록하시겠습니까?')) {
+      dispatch(addPost({ title: title }));
+      alert('등록되었습니다.');
+      onClickPostList();
+    } else {
+      alert('등록이 취소되었습니다.');
+    }
+  };
+
+  const editorRef = useRef();
   return (
     <div>
       <StHeader>
@@ -43,21 +71,22 @@ function PostForm() {
         <p>의자</p>
         <p>책상</p>
       </StItemSlider>
+      <input></input>
       <Editor
+        ref={editorRef}
         theme="dark"
-        initialValue="글 작성 가자잇!"
+        initialValue={title}
         previewStyle="vertical"
         height="800px"
         initialEditType="markdown"
         useCommandShortcut={false}
         hideModeSwitch={true}
         language="ko-KR"
+        onChange={onChange}
       />
       <StPostFormButton>
-        <button className="btn-15" onClick={onRemove}>
-          뒤로가기
-        </button>
-        <button className="btn-15">작성완료</button>
+        <button onClick={onRemove}>뒤로가기</button>
+        <button onClick={onClick}>작성완료</button>
       </StPostFormButton>
     </div>
   );
