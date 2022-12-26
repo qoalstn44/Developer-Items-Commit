9f00cd7a2738abef85418b449842b7d37998f9b3
Kidongg <dlrlehd7@daum.net>
2022-12-27 01:33:23 +0900

    Update Router component
    Route Header and Footer

--- src/Pages/Router.js
+++ src/Pages/Router.js
@@ -1,18 +1,21 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import MainPage from "../Pages/MainPage";
-import PostList from "../Pages/PostList";
-import PostForm from "../Pages/PostForm";
-import Post from "../Pages/Post";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from '../Pages/MainPage';
+import PostList from '../Pages/PostList';
+import PostForm from '../Pages/PostForm';
+import Post from '../Pages/Post';
+import Layout from '../components/main/Layout';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/postform" element={<PostForm />} />
-        <Route path="/postlist" element={<PostList />} />
-        <Route path="/post/:id" element={<Post />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/postform" element={<PostForm />} />
+          <Route path="/postlist" element={<PostList />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
