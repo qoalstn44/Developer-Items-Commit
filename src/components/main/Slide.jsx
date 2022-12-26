b7828b0857f040400f74c229bb08c44822517c2c
Kidongg <dlrlehd7@daum.net>
2022-12-26 10:06:08 +0900

    Fix css Slide component

--- src/components/main/Slide.jsx
+++ src/components/main/Slide.jsx
@@ -1,53 +1,82 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+
 import styled from 'styled-components';
+
 import logo from '../../img/logo.png';
-import { useState } from 'react';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-
-// SwiperCore.use([Navigation, Pagination]);
-// import 'swiper/swiper.scss';
-// import 'swiper/components/navigation/navigation.scss';
-// import 'swiper/components/pagination/pagination.scss';
-
-function Slide() {
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
-    </>
-    // <StItemSlide>
-    //   <div>
-    //     <p>컴퓨터</p>
-    //     <img src={logo} alt="logo" />
-    //   </div>
-    //   <div>
-    //     <p>키보드</p>
-    //     <img src={logo} alt="logo" />
-    //   </div>
-    //   <div>
-    //     <p>마우스</p>
-    //     <img src={logo} alt="logo" />
-    //   </div>
-    // </StItemSlide>
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'black' }}
+      onClick={onClick}
+    />
   );
 }
 
-const StItemSlide = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        background: 'black',
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+class Slide extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      // vertical: true,
+      // verticalSwiping: true,
+    };
+    return (
+      <StContainer>
+        <Slider {...settings}>
+          <StImg src={logo}></StImg>
+          <StImg src={logo}></StImg>
+          <StImg src={logo}></StImg>
+          <StImg src={logo}></StImg>
+          <StImg src={logo}></StImg>
+          <StImg src={logo}></StImg>
+        </Slider>
+      </StContainer>
+    );
+  }
+}
+
+const StContainer = styled.div`
+  border: 1px solid black;
+  margin: auto;
+  margin-bottom: 40px;
+  width: 90%;
+`;
+
+const StImg = styled.img`
+  /* margin: 10px; */
+  /* border: 1px solid black; */
+  /* border-right: 1px solid black; */
+  /* overflow: block; */
+  :hover {
+    background-color: #48af48;
+    border-radius: 4px;
+  }
 `;
 
 export default Slide;
