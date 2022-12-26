aaa5bd9284bc3f9d0b482350b33d1e0e9ec7b9d9
Kidongg <dlrlehd7@daum.net>
2022-12-26 10:06:35 +0900

    Split components by Mainpage

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../img/logo.png';
 import Header from '../components/main/Header';
 import Slide from '../components/main/Slide';
 import { useState } from 'react';
+import BestView from '../components/main/BestView';
+import SideBanner from '../components/main/SideBanner';
+import Footer from '../components/main/Footer';
 
 function MainPage() {
   const [toggle, setToggle] = useState(false);
@@ -16,45 +18,16 @@ function MainPage() {
       <Slide />
 
       <hr />
-      <StBestViewList>
-        <img src={logo} alt="logo" />
-        <img src={logo} alt="logo" />
-        <img src={logo} alt="logo" />
-      </StBestViewList>
+
+      <BestView />
+
       <hr />
-      <StSideBanner>
-        공식문서API, 라이브러리, 튜토리얼, 예제, 커뮤니티 등을 한눈에 볼 수 있는
-      </StSideBanner>
+      <SideBanner />
+
       <hr />
-      <StFooter>
-        팀이름, 팀소개, 팀원소개, 팀원이름, 팀원이메일, 팀원이미지, 팀원소개글,
-      </StFooter>
+      <Footer />
     </>
   );
 }
-
-////////// BestView  //////////
-const StBestViewList = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-`;
-
-////////// SideBanner //////////
-const StSideBanner = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-`;
-
-////////// Footer  //////////
-const StFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-`;
 
 export default MainPage;
