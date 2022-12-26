9d2ddf23539a50d457365d611fe371456b522700
Kidongg <dlrlehd7@daum.net>
2022-12-26 21:59:49 +0900

    Update Slide
    Add title, img
    Fix layout

--- src/components/main/Slide.jsx
+++ src/components/main/Slide.jsx
@@ -5,8 +5,11 @@ import React, { Component } from 'react';
 import Slider from 'react-slick';
 
 import styled from 'styled-components';
-
 import logo from '../../img/logo.png';
+
+// TODO : 버튼 스타일 수정
+// import { ReactComponent as Next } from '../../img/arrow-right-solid.svg';
+// import { ReactComponent as Prev } from '../../img/arrow-left-solid.svg';
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -41,42 +44,86 @@ class Slide extends Component {
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
+
       nextArrow: <SampleNextArrow />,
       prevArrow: <SamplePrevArrow />,
-      // vertical: true,
-      // verticalSwiping: true,
+
+      // nextArrow: () => (<Next />),
+      // prevArrow: () => (<Prev />),
+
+      speed: 1000,
+      responsive: [
+        {
+          nextArrow: false,
+          prevArrow: false,
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 1000,
+          },
+        },
+      ],
     };
+
+    // Todo : 동적 라우팅 구성
     return (
       <StContainer>
         <Slider {...settings}>
-          <StImg src={logo}></StImg>
-          <StImg src={logo}></StImg>
-          <StImg src={logo}></StImg>
-          <StImg src={logo}></StImg>
-          <StImg src={logo}></StImg>
-          <StImg src={logo}></StImg>
+          <StCommunityBox>
+            <StTitle>커뮤1</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
+          <StCommunityBox>
+            <StTitle>커뮤2</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
+          <StCommunityBox>
+            <StTitle>커뮤3</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
+          <StCommunityBox>
+            <StTitle>커뮤4</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
+          <StCommunityBox>
+            <StTitle>커뮤5</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
+          <StCommunityBox>
+            <StTitle>커뮤6</StTitle>
+            <StImg src={logo}></StImg>
+          </StCommunityBox>
         </Slider>
       </StContainer>
     );
   }
 }
 
-const StContainer = styled.div`
-  border: 1px solid black;
-  margin: auto;
-  margin-bottom: 40px;
-  width: 90%;
+const StTitle = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  border-bottom: 1px solid black;
 `;
 
 const StImg = styled.img`
-  /* margin: 10px; */
-  /* border: 1px solid black; */
-  /* border-right: 1px solid black; */
-  /* overflow: block; */
+  width: 100%;
   :hover {
     background-color: #48af48;
     border-radius: 4px;
   }
+`;
+
+const StCommunityBox = styled.div`
+  margin: auto;
+  text-align: center;
+`;
+
+const StContainer = styled.div`
+  border: 1px solid black;
+  margin: auto;
+  margin-bottom: 40px;
+  width: 80%;
 `;
 
 export default Slide;
