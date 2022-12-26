e8839995f2fa0eba20c87eeba0472ae18e2807ad
Kidongg <dlrlehd7@daum.net>
2022-12-27 01:30:59 +0900

    Update Header component
    Route global
    Route community
    Fix layout
    Fix toggle handler

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -11,6 +11,16 @@ const Header = ({ toggle, setToggle }) => {
     setToggle(!toggle);
   };
 
+  const onClickNavigateCommunityHandler = () => {
+    setToggle(!toggle);
+    navigate('/postlist');
+  };
+
+  const onClickNavigateLoginHandler = () => {
+    setToggle(!toggle);
+    navigate('/1');
+  };
+
   // const navMeun = ['커뮤니티', '마이페이지', '회원가입'];
 
   return (
@@ -19,7 +29,7 @@ const Header = ({ toggle, setToggle }) => {
         <img src={logo} alt="logo" onClick={() => navigate('/')} />
       </StNavLogo>
 
-      <StNavMenu className={'navbar-menu'}>
+      <StNavMenu>
         {/* Todo : 동적 라우팅 구성 */}
         {/* {navMeun.map((item, index) => {
           return (
@@ -28,14 +38,14 @@ const Header = ({ toggle, setToggle }) => {
             </li>
           );
         })} */}
-        <li onClick={() => navigate('/1')}>커뮤니티</li>
-        <li onClick={() => navigate('/2')}>회원가입/로그인</li>
+        <li onClick={() => navigate('/postlist')}>커뮤니티</li>
+        <li onClick={() => navigate('/1')}>회원가입/로그인</li>
       </StNavMenu>
 
       {toggle ? (
-        <StNavMenuNone className={'navbar-menu'}>
-          <li onClick={() => navigate('/1')}>커뮤니티</li>
-          <li onClick={() => navigate('/2')}>회원가입/로그인</li>
+        <StNavMenuNone>
+          <li onClick={onClickNavigateCommunityHandler}>커뮤니티</li>
+          <li onClick={onClickNavigateLoginHandler}>회원가입/로그인</li>
         </StNavMenuNone>
       ) : null}
 
@@ -46,12 +56,15 @@ const Header = ({ toggle, setToggle }) => {
   );
 };
 
+// TODO : 로고 가운데 정렬
 const StHeader = styled.nav`
   display: flex;
-  justify-content: flex-end;
   align-items: center;
+  //// 지울 곳 ////
+  justify-content: flex-end;
   gap: 200px;
   max-width: 1360px;
+  //// 지울 곳 ////
   margin: auto;
   border: 1px solid black;
   width: 80%;
@@ -70,8 +83,11 @@ const StNavLogo = styled.div`
 `;
 
 const StNavMenu = styled.div`
-  display: flex;
+  //// 지울 곳 ////
   list-style: none;
+  display: flex;
+  flex-direction: row;
+  //// 지울 곳 ////
   li {
     padding: 8px 12px;
   }
