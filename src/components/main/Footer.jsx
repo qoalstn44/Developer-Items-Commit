393dfe9e1c503a2ecb89c59f2330e0485bb5e573
Kidongg <dlrlehd7@daum.net>
2022-12-26 21:58:36 +0900

    Update footer
    add team information

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => {
-  return <StFooter>팀이름, 팀소개, 팀원소개, 팀원이름, 팀원이메일, 팀원이미지, 팀원소개글,</StFooter>;
+  return (
+    <StFooter>
+      <h1>팀 이름 : 인연</h1>
+      <div>
+        <p>
+          팀 소개 : 우리의 만남은 인연이었다. 4명의 남자들의 고군분투 프로젝트
+        </p>
+        <div>멤버 소개 : 김인섭, 박지수, 배민수, 이기동</div>
+      </div>
+    </StFooter>
+  );
 };
 
 const StFooter = styled.div`
+  width: 80%;
+  margin: auto;
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  gap: 100px;
 `;
+
+// const StFooterIntro = styled.div`
+//   padding: 10px;
+// `;
 
 export default Footer;
