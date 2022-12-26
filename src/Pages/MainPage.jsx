65915cd129ba0df501f83e025caf8e536c7da924
Kidongg <dlrlehd7@daum.net>
2022-12-27 01:32:51 +0900

    Update MainPage component
    Fix layout

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import Header from '../components/main/Header';
 import Slide from '../components/main/Slide';
-import { useState } from 'react';
 import BestView from '../components/main/BestView';
 import SideBanner from '../components/main/SideBanner';
-import Footer from '../components/main/Footer';
 
 function MainPage() {
-  const [toggle, setToggle] = useState(false);
-
   return (
     // 메인 홈페이지 컴포넌트 분리
     <>
-      <Header toggle={toggle} setToggle={setToggle} />
       <hr />
 
       <Slide />
@@ -25,7 +19,6 @@ function MainPage() {
       <SideBanner />
 
       <hr />
-      <Footer />
     </>
   );
 }
