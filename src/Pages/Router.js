e379f3277417bebf70158abbef011aae774d68a3
Kidongg <dlrlehd7@daum.net>
2022-12-27 17:12:38 +0900

    Update Route

--- src/Pages/Router.js
+++ src/Pages/Router.js
@@ -3,7 +3,7 @@ import MainPage from '../Pages/MainPage';
 import PostList from '../Pages/PostList';
 import PostForm from '../Pages/PostForm';
 import Post from '../Pages/Post';
-import Layout from '../components/main/Layout';
+import Layout from '../components/mainpage/Layout';
 import AuthTest from './AuthTest';
 import React from 'react';
 import ProfilePage from './ProfilePage';
@@ -14,9 +14,7 @@ const AppRouter = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<MainPage />} />
-
           <Route path="/authform" element={<AuthTest />} />
-
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/postform" element={<PostForm />} />
           <Route path="/postlist" element={<PostList />} />
