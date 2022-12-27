799e0b69013feb6b20706d1af152d508a76a261d
kimtanook <seob6402@gmail.com>
2022-12-27 10:20:20 +0900

    Update Redux persist

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import postModule from '../modules/postModule';
 import commentModule from '../modules/commentModule';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const postPersistedReducer = persistReducer(persistConfig, postModule);
+const commentPersistedReducer = persistReducer(persistConfig, commentModule);
+
 const store = configureStore({
-  reducer: { postModule: postModule, commentModule: commentModule },
+  reducer: {
+    postModule: postPersistedReducer,
+    commentModule: commentPersistedReducer,
+  },
+  middleware: [thunk],
 });
 
 export default store;
