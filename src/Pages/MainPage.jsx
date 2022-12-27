9aa5b23b40785583121fefb22f421c14c3ce84e6
Kidongg <dlrlehd7@daum.net>
2022-12-28 00:50:38 +0900

    Update MainPage
    Fix mobile header

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Slide from '../components/mainpage/Slide';
 import BestView from '../components/mainpage/BestView';
+import styled from 'styled-components';
 
 function MainPage() {
   return (
-    // 메인 홈페이지 컴포넌트 분리
-    <>
+    <StMain>
       <Slide />
 
       <hr />
 
       <BestView />
-    </>
+    </StMain>
   );
 }
+
+const StMain = styled.div`
+  @media screen and (max-width: 768px) {
+    padding-top: 90px;
+  }
+`;
 
 export default MainPage;
