dc05f86334b425ff52f727d7561edb507db99a2c
Kidongg <dlrlehd7@daum.net>
2022-12-27 22:59:31 +0900

    Update Footer
    Fix layout

--- src/components/mainpage/Footer.jsx
+++ src/components/mainpage/Footer.jsx
@@ -3,13 +3,16 @@ import styled from 'styled-components';
 
 const Footer = () => {
   return (
-    <StFooter>
-      <h1>인연</h1>
-      <div>
-        <p>인연 소개 : 인연을 중요시 생각하는 남자들의 고군분투 프로젝트</p>
-        <div>멤버 소개 : 김인섭, 박지수, 배민수, 이기동</div>
-      </div>
-    </StFooter>
+    <>
+      <hr />
+      <StFooter>
+        <h1>인연</h1>
+        <div>
+          <p>인연 소개 : 인연을 중요시 생각하는 남자들의 고군분투 프로젝트</p>
+          <div>멤버 소개 : 김인섭, 박지수, 배민수, 이기동</div>
+        </div>
+      </StFooter>
+    </>
   );
 };
 
